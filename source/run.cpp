3b59f8e0cbc126735ee155cf993e692e04457f49
Jakob Ruckel <jakob@ruckel.de>
2022-05-22 13:48:39 +0200

    run: Add missing commands

--- source/run.cpp
+++ source/run.cpp
@@ -12,6 +12,10 @@ int main() {
       std::cout << "\ta\tadd to tree\n";
       std::cout << "\ts\tsearch in tree\n";
       std::cout << "\tr\tremove from tree\n";
+      std::cout << "\tpd\tprint predecessor\n";
+      std::cout << "\tsc\tprint successor\n";
+      std::cout << "\tmin\tprint minimum\n";
+      std::cout << "\tmax\tprint maximum\n";
       std::cout << "\tp\tprint tree to file (- for stdout)\n";
       std::cout << "\tsize\tprint size\n";
       std::cout << "\tq\tquit\n";
@@ -40,6 +44,32 @@ int main() {
         b.remove(node);
       else
         std::cout << "not in tree!\n";
+    }
+    if (cmd == "pd" || cmd == "sc") {
+      std::string what = cmd == "pd" ? "predecessor" : "successor";
+      std::cout << "enter a number to find " << what << " of: ";
+      int n;
+      std::cin >> n;
+      BSTNode* node = b.search(n);
+      if (nullptr == node) {
+        std::cout << "not found\n";
+      } else {
+        auto node_what = cmd == "pd" ? b.pred(node) : b.succ(node);
+        if (nullptr == node_what) {
+          std::cout << "No " << what << " found\n";
+        } else {
+          std::cout << what << ": " << node_what->value << "\n";
+        }
+      }
+    }
+    if (cmd == "min" || cmd == "max") {
+      std::string what = cmd == "min" ? "minimum" : "maximum";
+      BSTNode* node_what = cmd == "min" ? b.min() : b.max();
+      if (nullptr == node_what) {
+          std::cout << "No " << what << " found\n";
+      } else {
+          std::cout << what << ": " << node_what->value << "\n";
+      }
     }
     if (cmd == "p") {
       std::cout << "enter a filename (- for stdout): ";
